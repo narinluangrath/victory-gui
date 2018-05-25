644614a921f0af1ee1acb4f8a1cb69ae76996c87
Narin Luangrath <narin.luangrath@gmail.com>
2018-05-25 16:45:15 -0400

    Build: initial commit - set up react

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+const container = document.getElementById( 'container' )
+ReactDOM.render( <div>Hello World</div>, container )
