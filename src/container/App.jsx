b4ac9ace19ad1ee48af389f0b0eb234076a3b291
Narin Luangrath <narin.luangrath@gmail.com>
2018-06-05 23:28:00 -0400

    Build: whatever

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,40 +1,21 @@
 import React, { Component, Fragment } from 'react'
 
 import Header from '../presentational/Header.jsx'
-import DesignView from './DesignView.jsx'
+import Body from './Body.jsx'
 import CodeView from '../presentational/CodeView.jsx'
 import './App.css'
-
-// constants for state variables
-const DESIGN = 'DESIGN'
-const CODE = 'CODE'
 
 class App extends Component {
 
 	constructor( props ) {
 		super( props )
-		this.state = { 
-			view : DESIGN 
-		}
-	}
-
-	switchView() {
-		this.setState( prevState => {
-			const newView = prevState.view === DESIGN ? CODE : DESIGN
-			return { view : newView }
-		})
 	}
 
 	render() {
 		return (
 			<div className='appContainer'>
 				<Header />
-{/*				<div className='main'>
-					{ this.state.view === DESIGN ? 
-		 				<DesignView /> : <CodeView />
-					}
-				</div>
-*/}
+				<Body />
 			</div>
 		)
 	}
--- src/presentational/CodeView.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function CodeView( props ) {
-	return <div/>
-}
-
-export default CodeView
