82c757d4a7724cef0c1470b4486adcff55e03417
Narin Luangrath <narin.luangrath@gmail.com>
2018-06-05 21:36:08 -0400

    Build: header

--- src/presentational/Header.jsx
+++ src/presentational/Header.jsx
@@ -2,23 +2,44 @@ import React from 'react'
 
 import './Header.css'
 import ChartIcon from '../icons/chart.svg'
+import GithubIcon from '../icons/github.svg'
+import InfoIcon from '../icons/info.svg'
 
-const Dividor = ( props ) => {
-	const { space } = props 
+const Dividor = ( { space } ) => {
 	const margin = space == null ? 
 								 `0px 10px 0px 10px` : 
 								 `0px ${space}px 0px ${space}px`
 	return (
-		<div style={{width: 0, height: '20px', margin, borderRight: '1px solid rgba(255, 255, 255, 0.3)'}} />
+		<div className='dividor' style={{margin}} />
+	)
+}
+
+const Link = ( { children, href } ) => {
+	return (
+		<a className='link' href={href}> 
+			{children}
+		</a>
 	)
 }
 
 const Header = ( props ) => {
 	return (
-		<div className='header'> 
-			<ChartIcon fill='white' />
-			<Dividor space={15} />
-			<p>Victory Chart Design Studio</p>
+		<div className='header'>
+			<div className='left'> 
+				<ChartIcon fill='white' />
+				<Dividor space={15} />
+				<p>Victory Chart Design Studio</p>
+			</div>
+			<div className='right'>
+				<Link href='http://github.com'>
+					<GithubIcon />
+					<p>Source</p>
+				</Link>
+				<Link href='http://www.narinluangrath.com'>
+					<InfoIcon />
+					<p>About</p>
+				</Link>
+			</div>
 		</div>
 	)
 }
